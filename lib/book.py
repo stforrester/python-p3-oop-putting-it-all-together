bc32a621e458aa83b627e007e4279718f7bbfaff
Stuart Forrester <stforrester@gmail.com>
2025-01-02 04:24:19 -0600

    completes the lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title="The Grapes of Wrath", page_count=464):
+         self.title = title
+         self.page_count = page_count
     
+    @property
+    def page_count(self):
+         """The page_count property"""
+         return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        """Page_count must be an integer"""
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand="Blundstone", size=10):
+        self.brand = brand
+        self.size = size
+    
+    @property
+    def size(self):
+        """The size property"""
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        """Size must be an integer"""
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
